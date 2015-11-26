c0c0acf1be23b1ebd0e7df2acb3a8b17549150e2
Bruno Martin <bruno.martin@hacklab.com.br>
2015-11-26 20:17:09 -0200

    adicionadas configurações no arquivo de exemplo

--- config.example.js
+++ config.example.js
@@ -1,3 +1,5 @@
+// More examples here: https://github.com/CartoDB/Windshaft-cartodb/blob/master/config/environments/
+
 module.exports = {
   port: 4000,
   postgres: {
@@ -11,6 +13,13 @@ module.exports = {
     host: '127.0.0.1',
     port: 6379
   },
+  // Seconds since "last creation" before a detached
+  // or template instance map expires. Or: how long do you want
+  // to be able to navigate the map without a reload ?
+  // Defaults to 7200 (2 hours)
+  mapConfigTTL: 7200,
+  // idle socket timeout, in milliseconds
+  socket_timeout: 600000.
   geometry_field: 'polygon',
   cache_basedir: '/tmp/windshaft/millstone',
   land_tenures_api: 'http://localhost:8000/api/land_tenures',
